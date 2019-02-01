41ebde1008837282a5e01c8597d0dd6be9d21fd6
Daniel <danielsosa@gmail.com>
2019-02-01 20:00:52 +0100

    rock falling in highway implemented

---
+++ Assets/RockFall.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockFall : MonoBehaviour
+{
+    public Rigidbody rocks;
+    public Transform instantiatePosition;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Instantiate(rocks, instantiatePosition.position, instantiatePosition.rotation);
+        }
+    }
+}
